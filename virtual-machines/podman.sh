435e1b0b90b6e02e59d413bc57a2a8a47e6a0d83
platnub <81484914+platnub@users.noreply.github.com>
2025-08-24 00:34:43 +0200

    Update podman.sh

--- virtual-machines/podman.sh
+++ virtual-machines/podman.sh
@@ -571,8 +571,7 @@ msg_ok "Podman installed"
 if [ "$OPEN_PORTS" = "yes" ]; then
   msg_info "Configuring privileged ports 80+ for Podman containers"
   virt-customize -q -a "${FILE}" --run-command "sudo /bin/su -c \"echo -e '# Lowering privileged ports to 80 to allow us to run rootless Podman containers on lower ports\n# default: 1024\nnet.ipv4.ip_unprivileged_port_start=80' >> /etc/sysctl.d/podman-privileged-ports.conf\"" >/dev/null &&
-#  virt-customize -q -a "${FILE}" --run-command "sudo sysctl --load /etc/sysctl.d/podman-privileged-ports.conf" >/dev/null
-sysctl --load /etc/sysctl.d/podman-privileged-ports.conf
+  virt-customize -q -a "${FILE}" --firstboot-command "sudo sysctl --load /etc/sysctl.d/podman-privileged-ports.conf" >/dev/null
   msg_ok "Configuring privileged ports 80+ for Podman containers"
 else
   msg_ok "Skipping privileged ports 80+ configuration for Podman containers"
