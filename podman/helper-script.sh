04d0fba55d08cffc6a4aff40b866a29217153a1f
platnub <81484914+platnub@users.noreply.github.com>
2025-08-24 17:39:48 +0200

    Update helper-script.sh

--- podman/helper-script.sh
+++ podman/helper-script.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 base_dir="/home/podman/containers"
 
+# Function to list all containers
+list_containers() {
+    echo "Listing all Podman containers:"
+    podman ps -a
+}
+
+# Function to run a container
+run_container() {
+    local container_name=$1
+    podman-compose --file "$base_dir/$container_name/compose.yaml" up --detach
+    echo "Container $container_name started successfully."
+}
+
+# Function to stop a container
+stop_container() {
+    read -p "Enter the container name to stop: " container_name
+    podman-compose --file "$base_dir/$container_name/compose.yaml" down
+    echo "Container $container_name stopped successfully."
+}
+
 # Function to create a new container
 create_container() {
     read -p "Enter the container name: " container_name
@@ -30,15 +50,7 @@ create_container() {
     sudo /bin/su -c "echo -e \"PUID=1000\nPGID=1000\nTZ=\"Europe/Amsterdam\"\nDOCKERDIR=\"$base_dir\"\nDATADIR=\"$base_dir/$container_name/appdata\"\" > '$base_dir/$container_name/.env'"
     sudo ${EDITOR:-nano} "$base_dir/$container_name/.env"
 
-    # Apply user permissions
-    sudo chmod 700 "$base_dir/$container_name"
-    sudo chmod 700 "$base_dir/$container_name/appdata"
-    sudo chmod 700 "$base_dir/$container_name/logs"
-    sudo chmod 400 "$base_dir/$container_name/secrets"
-    sudo chmod 400 "$base_dir/$container_name/compose.yaml"
-    sudo chmod 400 "$base_dir/$container_name/.env"
-    sudo chown -R podman:podman "$base_dir/$container_name"
-    podman unshare chown -R 1000:1000 "$base_dir/$container_name/appdata"
+    reapply_permissions
     
     echo "Container $container_name created successfully with user."
 
@@ -48,62 +60,60 @@ create_container() {
         run_container "$container_name"
     fi
 }
-# Function to run a container
-run_container() {
-    local container_name=$1
-    podman-compose --file "$base_dir/$container_name/compose.yaml" up --detach
-    echo "Container $container_name started successfully."
+
+    # Apply user permissions
+    reapply_permissions() {
+    sudo chmod 700 "$base_dir/$container_name"
+    sudo chmod 700 "$base_dir/$container_name/appdata"
+    sudo chmod 700 "$base_dir/$container_name/logs"
+    sudo chmod 400 "$base_dir/$container_name/secrets"
+    sudo chmod 400 "$base_dir/$container_name/compose.yaml"
+    sudo chmod 400 "$base_dir/$container_name/.env"
+    sudo chown -R podman:podman "$base_dir/$container_name"
+    podman unshare chown -R 1000:1000 "$base_dir/$container_name/appdata"
+    echo "Permissions applied succesfully."
 }
 
-# Function to stop a container
-stop_container() {
-    read -p "Enter the container name to stop: " container_name
-    podman-compose --file "$base_dir/$container_name/compose.yaml" down
-    echo "Container $container_name stopped successfully."
-}
-
-# Function to list all containers
-list_containers() {
-    echo "Listing all Podman containers:"
-    podman ps -a
-}
-
-# Function to remove a container
-remove_container() {
-    read -p "Enter the container name to remove: " container_name
-#    stop_container $container_name
-    podman rm "$container_name"
-    sudo rm -rf "$base_dir/$container_name"
-    echo "Container $container_name removed successfully."
+    # Function to remove a container
+    remove_container() {
+        read -p "Enter the container name to remove: " container_name
+    #    stop_container $container_name
+        podman rm "$container_name"
+        sudo rm -rf "$base_dir/$container_name"
+        echo "Container $container_name removed successfully."
 }
 
 # Main menu
 while true; do
     echo "Podman Container Management Menu"
-    echo "1. Create a new container"
-    echo "2. List all containers"
-    echo "3. Remove a container"
-    echo "4. Run a container"
-    echo "5. Stop a container"
+    echo "1. List all containers"
+    echo "2. Run a container"
+    echo "3. Stop a container"
+    echo "4. Create a new container"
+    echo "5. Reapply permissions"
+    echo "99. Remove a container"
     echo "6. Exit"
     read -p "Enter your choice (1-6): " choice
 
     case $choice in
         1)
-            create_container
-            ;;
-        2)
             list_containers
             ;;
-        3)
-            remove_container
-            ;;
-        4)
+        2)
             read -p "Enter the container name to run: " container_name
             run_container "$container_name"
             ;;
-        5)
+        3)
             stop_container
+            ;;
+        4)
+            create_container
+            ;;
+        4)
+            reapply_permissions
+            ;;
+        99)
+            remove_container
             ;;
         6)
             echo "Exiting..."
