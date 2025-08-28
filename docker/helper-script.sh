2541cbdb3fb873cff51106342408b31a880162ab
platnub <81484914+platnub@users.noreply.github.com>
2025-08-28 15:23:37 +0200

    Update helper-script.sh

--- docker/helper-script.sh
+++ docker/helper-script.sh
@@ -48,7 +48,6 @@ install_argon2() {
     return 0
 }
 
-# Function to generate a secret using argon2
 generate_secret() {
     local secret_string=$1
     if [ -z "$secret_string" ]; then
@@ -56,13 +55,18 @@ generate_secret() {
         secret_string=$(openssl rand -base64 32)
     fi
 
+    # Escape $ characters in the secret string
+    secret_string=${secret_string//\$/$$}
+
     # Hash the string with argon2
     local hashed_secret=$(argon2 "$secret_string" -e -id -k 19456 -t 2 -p 1 2>/dev/null)
-
     if [ -z "$hashed_secret" ]; then
         error_msg "Failed to generate secret with argon2."
         return 1
     fi
+
+    # Escape $ characters in the hashed secret
+    hashed_secret=${hashed_secret//\$/$$}
 
     echo "$hashed_secret"
     return 0
